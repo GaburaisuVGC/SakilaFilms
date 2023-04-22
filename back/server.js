a1f3809c9ff2a74557b4215fcedfaf6609151722
gaburaisu <56201393+GaburaisuVGC@users.noreply.github.com>
2023-04-22 11:22:11 +0200

    Netlify

--- back/server.js
+++ back/server.js
@@ -54,7 +54,7 @@ app.get("/films", (req, res) => {
 
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
